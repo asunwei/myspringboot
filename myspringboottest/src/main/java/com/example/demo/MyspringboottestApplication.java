a5653b9ca65b706dd1d53da9499aef1b8bdc4ded
asunwei <993525033@qq.com>
2017-08-02 17:38:58 +0800

    first commit

---
+++ myspringboottest/src/main/java/com/example/demo/MyspringboottestApplication.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyspringboottestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyspringboottestApplication.class, args);
+	}
+}
