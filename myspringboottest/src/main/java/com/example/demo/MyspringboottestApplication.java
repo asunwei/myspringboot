ebed140ac924efaa32709c388d7f46f0a2d53494
asunwei <993525033@qq.com>
2017-08-02 18:41:58 +0800

    add controller

--- myspringboottest/src/main/java/com/example/demo/MyspringboottestApplication.java
+++ myspringboottest/src/main/java/com/example/demo/MyspringboottestApplication.java
@@ -2,6 +2,7 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
 public class MyspringboottestApplication {
@@ -9,4 +10,9 @@ public class MyspringboottestApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(MyspringboottestApplication.class, args);
 	}
+	
+	 @GetMapping("/api/hello")
+	    public String hello() {
+	        return "Hello From Spring Boot";
+	    }
 }
